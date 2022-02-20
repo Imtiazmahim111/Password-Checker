bcd85e32b57dec3f898601375efee68121bc8463
Imtiaz uddin mahim <imtiazmahim111@gmail.com>
2022-02-20 16:10:02 +0600

    The HMLT, CSS of the project is Complete

---
+++ script.js
@@ -0,0 +1,7 @@
+const password_1 = document.querySelector('#password_1');
+const password_2 = document.querySelector('#password_2');
+const error_text = document.querySelector('.error-text');
+const showBtn = document.querySelector('.show');
+const button = document.querySelector('button');
+   
+    
